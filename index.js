af8e330fe7b278046ccfa407f51eb30296a538ea
Scott Bender <scott@scottbender.net>
2017-07-26 18:47:20 -0400

    Don't read the settings file every time

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const _ = require('lodash')
 
 module.exports = function(app) {
   var plugin = {}
-  var defaultsFile
+  var gaugesFile
+  var gauges
 
   plugin.id = "wilhelmsk-plugin"
   plugin.name = "WilhelmSK Plugin"
-  plugin.description = "Signal K node server plugin to allow clients to update the defaults.json file"
+  plugin.description = "Signal K node server plugin that's provides special functionality for WilhelmSK"
 
   plugin.schema = {
     title: "WilhelmSK Plugin",
@@ -22,24 +23,26 @@ module.exports = function(app) {
 
   plugin.start = function(options) {
     var dir = app.config.configPath || app.config.appPath
-    defaultsFile = path.join(dir, 'plugin-config-data/wilhelmsk-gauges.json');
-    debug("gauges file: " + defaultsFile)
+    gaugesFile = path.join(dir, 'plugin-config-data/wilhelmsk-gauges.json');
+    debug("gauges file: " + gaugesFile)
+
+    gauges = readGauges()
     return true
   }
 
   function readGauges() {
-    var defaults = {}
-    if ( fs.existsSync(defaultsFile) ) {
-      var defaultsString = fs.readFileSync(defaultsFile)
-      defaults = JSON.parse(defaultsString);
+    var res = {}
+    if ( fs.existsSync(gaugesFile) ) {
+      var gaugesString = fs.readFileSync(gaugesFile)
+      res = JSON.parse(gaugesString);
     }
-    return defaults
+    return res
   }
 
   function saveGauges(gauges, done) {
     var json = JSON.stringify(gauges, null, 2)
     
-    fs.writeFile(defaultsFile, json,
+    fs.writeFile(gaugesFile, json,
                  function(err) {
                    if (err) {
                      debug(err.stack)
@@ -57,7 +60,6 @@ module.exports = function(app) {
 
   plugin.registerWithRouter = function(router) {
     router.get("/get/gauges", (req, res) => {
-      var gauges = readGauges()
       res.json(gauges)
     });
 
@@ -66,7 +68,6 @@ module.exports = function(app) {
       
       debug("title: " + title);
 
-      var gauges = readGauges()
       var gauge = gauges[title]
       if ( typeof gauge !== 'undefined' ) {
         delete gauges[title]
@@ -99,8 +100,6 @@ module.exports = function(app) {
         res.send("Invalid Request")
         return
       }
-
-      var gauges = readGauges()
 
       gauges[title] = gauge
 
