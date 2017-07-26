ed1430fbb130eaa12b206ff2d18727e4e50632d3
Scott Bender <scott@scottbender.net>
2017-07-26 15:47:54 -0400

    converted to wilhelmsk plugin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('defaults-updates')
+const debug = require('debug')('wilhelmsk-plugin')
 const util = require('util')
 const path = require('path')
 const fs = require('fs')
@@ -9,12 +9,12 @@ module.exports = function(app) {
   var plugin = {}
   var defaultsFile
 
-  plugin.id = "defaults-updates"
-  plugin.name = "Defaults Updater"
+  plugin.id = "wilhelmsk-plugin"
+  plugin.name = "WilhelmSK Plugin"
   plugin.description = "Signal K node server plugin to allow clients to update the defaults.json file"
 
   plugin.schema = {
-    title: "Defaults Updater",
+    title: "WilhelmSK Plugin",
     type: "object",
     properties: {
     }
@@ -22,12 +22,12 @@ module.exports = function(app) {
 
   plugin.start = function(options) {
     var dir = app.config.configPath || app.config.appPath
-    defaultsFile = path.join(dir, 'settings/defaults.json');
-    debug("defaultsFile: " + defaultsFile)
+    defaultsFile = path.join(dir, 'plugin-config-data/wilhelmsk-gauges.json');
+    debug("gauges file: " + defaultsFile)
     return true
   }
 
-  function readDefaults() {
+  function readGauges() {
     var defaults = {}
     if ( fs.existsSync(defaultsFile) ) {
       var defaultsString = fs.readFileSync(defaultsFile)
@@ -36,99 +36,83 @@ module.exports = function(app) {
     return defaults
   }
 
-  plugin.registerWithRouter = function(router) {
-    router.get("/get/*", (req, res) => {
-      var path = req.path.substring(5).replace(/\//g, '.')
-      debug("path: " + path);
-
-      var defaults = readDefaults()
-      var value = _.get(defaults, path)
-
-      if ( typeof value !== 'undefined' ) {
-        res.json(value)
-      } else {
-        res.status(404);
-        res.send("Not found");
-      }
-    });
-
-    router.get("/delete/*", (req, res) => {
-      var path = req.path.replace('/delete/', '').replace(/\//g, '.')
-      debug("path: " + path);
-
-      var defaults = readDefaults()
-      var value = _.get(defaults, path)
-
-      if ( typeof value !== 'undefined' ) {
-        _.unset(defaults, path, null)
-        var json = JSON.stringify(defaults, null, 2)
-        //debug("new defaults: " + json)
-        
-        fs.writeFile(defaultsFile, json,
-                     function(err) {
-                       if (err) {
-                         debug(err.stack)
-                         console.log(err)
-                         res.status(500)
-                         res.send(err)
-                         return
-                       }
-                       else
-                       {
-                         if ( path.startsWith('vessels.self') ) {
-                           sendDelta(path, null);
-                       }
-                         
-                       res.send("Default Removed")
-                       }
-                     });        
-      } else {
-        res.status(404);
-        res.send("Not found");
-      }
-    });
-
+  function saveGauges(gauges, done) {
+    var json = JSON.stringify(gauges, null, 2)
     
-    router.post("/save/*", (req, res) => {
-      var spath = req.path.substring(6).replace(/\//g, '.')
-      var value = req.body
+    fs.writeFile(defaultsFile, json,
+                 function(err) {
+                   if (err) {
+                     debug(err.stack)
+                     console.log(err)
+                     done(err)
+                     return
+                   }
+                   else
+                   {
+                     done()
+                   }
+                 });        
+    
+  }
 
-      debug("path: " + spath)
-      debug("value: " + JSON.stringify(value))
+  plugin.registerWithRouter = function(router) {
+    router.get("/get/gauges", (req, res) => {
+      var gauges = readGauges()
+      res.json(gauges)
+    });
+
+    router.post("/delete/gauge", (req, res) => {
+      var title = req.body.title
       
-      if ( typeof spath === 'undefined'
-           || typeof value === 'undefined')
+      debug("title: " + title);
+
+      var gauges = readGauges()
+      var gauge = gauges[title]
+      if ( typeof gauge !== 'undefined' ) {
+        delete gauges[title]
+        saveGauges(gauges, function(err) {
+          if (err) {
+            res.status(500)
+            res.send(err)
+            return
+          } else {
+            res.send("Gauge Removed")
+          }
+        });        
+      } else {
+        res.status(404);
+        res.send("Not found");
+      }
+    });
+
+   
+    router.post("/save/gauge", (req, res) => {
+      var gauge = req.body
+
+      debug("gauge: " + JSON.stringify(gauge))
+
+      var title = gauge.title;
+      
+      if ( typeof gauge === 'undefined' || typeof title == 'undefined')
       {
         res.status(400)
         res.send("Invalid Request")
         return
       }
 
-      var defaults = readDefaults()
+      var gauges = readGauges()
 
-      _.set(defaults, spath, value)
+      gauges[title] = gauge
 
-      var json = JSON.stringify(defaults, null, 2)
-      //debug("new defaults: " + json)
-
-      fs.writeFile(defaultsFile, json,
-                   function(err) {
-                     if (err) {
-                       debug(err.stack)
-                       console.log(err)
-                       res.status(500)
-                       res.send(err)
-                       return
-                     }
-                     else
-                     {
-                       if ( spath.startsWith('vessels.self') ) {
-                         sendDelta(spath, value);
-                       }
-                       
-                       res.send("Defaults Saved")
-                     }
-                   });
+      saveGauges(gauges, function(err) {
+        if (err) {
+          res.status(500)
+          res.send(err)
+          return
+        } else {
+          res.send("Gauge Saved")
+        }
+      });
       
     })
   }
